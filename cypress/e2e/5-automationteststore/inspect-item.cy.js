9a8e1573270c4ea1a5c66833d9db8937d8c4c8ac
Pawel Duminski <pduminski@gmail.com>
2023-06-11 18:44:59 +0200

    Adding then command test case

--- cypress/e2e/5-automationteststore/inspect-item.cy.js
+++ cypress/e2e/5-automationteststore/inspect-item.cy.js
@@ -16,7 +16,12 @@ describe("Inspect Automation Test Store items using cain of commands", () => {
     const homePage = new HomePage();
     homePage.visitPage();
 
-    cy.get(".prdocutname").contains("Skinsheen Bronzer Stick").click();
+    cy.get(".prdocutname")
+      .contains("Skinsheen Bronzer Stick")
+      .click()
+      .then(function (itemHeaderText) {
+        console.log("Selected the folowing item:" + itemHeaderText.text());
+      });
   });
 
   it("Click on the first item using index", () => {
@@ -24,5 +29,30 @@ describe("Inspect Automation Test Store items using cain of commands", () => {
     homePage.visitPage();
 
     cy.get(".fixed_wrapper").find(".prdocutname").eq(0).click();
+  });
+
+  it("Click on Contact Us and take the string out of it", () => {
+    const homePage = new HomePage();
+    homePage.visitPage();
+
+    cy.get(".info_links_footer")
+      .find(
+        "a[href='https://automationteststore.com/index.php?rt=content/contact']"
+      )
+      .click()
+      .then((linkName) => {
+        console.log("Found Text:" + linkName.text());
+      });
+  });
+
+  it.only("Click on Contact Us and take the string out of it", () => {
+    const homePage = new HomePage();
+    homePage.visitPage();
+
+    cy.get("a[href$='contact']")
+      .click()
+      .then((linkName) => {
+        console.log("Found Text:" + linkName.text());
+      });
   });
 });
