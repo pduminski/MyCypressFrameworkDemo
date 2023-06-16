8927cdd2568096a26f112fd856aece4582c30014
Pawel Duminski <pduminski@gmail.com>
2023-06-16 08:57:35 +0200

    adding ready excercises / finished

---
+++ cypress/e2e/other/same-origin-policy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress"/>
+
+describe("Cypress web security", () => {
+  it("Validate visiting two different domains", () => {
+    cy.visit("http://www.webdriveruniversity.com/");
+    cy.visit("https://www.automationteststore.com/");
+  });
+
+  it("Validate visiting two different domains via user actions", () => {
+    cy.visit("http://www.webdriveruniversity.com/");
+    cy.get("#automation-test-store").invoke("removeAttr", "target").click();
+  });
+
+  it("Origin Command", () => {
+    cy.origin("webdriveruniversity.com", () => {
+      cy.visit("/");
+    });
+
+    cy.origin("automationteststore.com", () => {
+      cy.visit("/");
+    });
+
+    cy.visit("https://www.webdriveruniversity.com");
+    cy.visit("https://www.selectors.webdriveruniversity.com");
+  });
+});
