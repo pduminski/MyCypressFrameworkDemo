7c27975ca360eebf172fc68bbd4ff442fb5a51d5
Pawel Duminski <pduminski@gmail.com>
2023-06-17 17:00:15 +0200

    cypress commands

--- cypress/e2e/5-automationteststore/iterate-over-elements.cy.js
+++ cypress/e2e/5-automationteststore/iterate-over-elements.cy.js
@@ -1,29 +1,21 @@
-import HomePage from "../../page-objects/5-automationteststore/homepage";
-
 describe("Iterate over elements", () => {
-  beforeEach(() => {});
-
-  it("Log information about all hair care products", () => {
-    const homePage = new HomePage();
-    homePage.visitPage();
-
+  it("Log information of all hair care products", () => {
+    cy.visit("https://automationteststore.com/");
     cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
 
     cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
-      cy.log("Index: ", index + " : " + $el.text());
+      cy.log("Index: " + index + " : " + $el.text());
     });
   });
-
-  it("Add specific product to cart", () => {
-    const homePage = new HomePage();
-    homePage.visitPage();
-
+  it("Add specific product to basket", () => {
+    cy.visit("https://automationteststore.com/");
     cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
 
-    cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
-      if ($el.text().includes("Eau Parfumee au The Vert Shampoo")) {
-        cy.wrap($el).click();
-      }
-    });
+    // cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
+    //   if ($el.text().includes("Curls to straight Shampoo")) {
+    //     cy.wrap($el).click();
+    //   }
+    // });
+    cy.selectProduct("Curls to straight Shampoo");
   });
 });
